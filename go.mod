e9d08766685a4951015ffe9c5144a9ec055154c1
LiDong <47240528+gcslaoli@users.noreply.github.com>
2023-03-15 14:43:41 +0000

    初始版本

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/gcslaoli/go-socks5-server
+
+go 1.20
+
+require (
+	github.com/armon/go-socks5 v0.0.0-20160902184237-e75332964ef5 // indirect
+	golang.org/x/net v0.8.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/armon/go-socks5"
+
+func main() {
+	conf := &socks5.Config{}
+	server, err := socks5.New(conf)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create SOCKS5 proxy on localhost port 8000
+	if err := server.ListenAndServe("tcp", "127.0.0.1:8000"); err != nil {
+		panic(err)
+	}
+}
